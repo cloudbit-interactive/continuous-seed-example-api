5a8ffebbe21011d06e769632461a47b6f39ae1ad
Tufik Chediak <tufik2@gmail.com>
2022-11-22 23:17:54 -0500

    no message

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { config } from '../config.js';
 
 const app = express();
 app.get('/ping', (req, res) => {
-  res.send('pong');
+  res.send('pong2');
 })
 app.get('/', (req, res) => {
   res.send(JSON.stringify(config, null, 2));
